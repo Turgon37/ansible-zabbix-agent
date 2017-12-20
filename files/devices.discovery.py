9cffbfe9d66d40b891d7e75be7a1cbe78c8356cd
Pierre GINDRAUD <pgindraud@gmail.com>
2017-12-20 21:32:39 +0100

    add regexp as ignore pattern

--- files/devices.discovery.py
+++ files/devices.discovery.py
@@ -6,22 +6,31 @@
 import argparse
 import json
 import os
+import re
 
 
 if __name__ == "__main__":
     # List of device to exclude from results
     exclude_device_names = ['sr', 'loop', 'ram']
+    exclude_device_names_pattern = []
 
     # CREATE PARSER
     parser = argparse.ArgumentParser(description="Search for block devices connected to this host")
     parser.add_argument('--exclude', action='append', dest='exclude_device_names', default=exclude_device_names,
+                          help='Append a device name to the excluded list. Each name is compare with host devices names\' using substring test')
+    parser.add_argument('--exclude-pattern', action='append', dest='exclude_device_names_pattern', default=None,
                           help='Append a device name to the excluded list')
     args = parser.parse_args()
+
+    if args.exclude_device_names_pattern:
+        for pattern in args.exclude_device_names_pattern:
+            exclude_device_names_pattern.append(re.compile(pattern))
 
     data = []
     # Iterate over all block devices, but ignore them if they are in the skippable set
     for device in os.listdir("/sys/class/block"):
-        if not any(ignore in device for ignore in args.exclude_device_names):
+        if ( not any(ignore in device for ignore in args.exclude_device_names) and
+              not any(regexp.match(device) for regexp in exclude_device_names_pattern) ):
             data.append({"{#DEVICENAME}": device})
 
     # output
